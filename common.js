05c6cbaa83994bd004a513f0561bf62b419b5191
CloneWith <clonewith@qq.com>
2024-07-11 13:36:19 +0800

    Auto formatting

--- common.js
+++ common.js
@@ -1,11 +1,20 @@
 /**
  * Common values for this site.
+ *
+ * Note: All relative paths are based on "static/".
  */
 const common = {
-    title: 'osu!dictionary',
-    description: '为 osu! 爱好者准备的简明词典',
-    icon: 'img/site-logo.png',
-
+  title: "osu!dictionary",
+  description: "为 osu! 爱好者准备的简明词典",
+  icon: "img/site-logo.png",
+  cardbanner: "img/card.png",
+  src: "https://github.com/osu-atri/osu-dictionary",
+  contact: {
+    github_org: "https://github.com/osu-atri",
+    github_join:
+      "https://github.com/osu-atri/.github/blob/main/profile/CONTRIBUTING.md",
+    bilibili: "https://space.bilibili.com/1313510214/",
+  },
 };
 
 module.exports = common;
